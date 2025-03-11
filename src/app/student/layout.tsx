bd8cfaec09ba157033da1c52bd2fbb7542d0a57c
pallavisaxena0245 <pallavisaxena0245@gmail.com>
2025-03-11 08:47:50 +0530

    made site functional

--- src/app/student/layout.tsx
+++ src/app/student/layout.tsx
@@ -3,7 +3,6 @@ import { ReactNode } from "react";
 export default function Layout({children} : {children: ReactNode}) {
     return (
         <div className="flex flex-col items-center w-full h-screen p-2 gap-2 lg:gap-4">
-          
             {children}          
         </div>
     )
