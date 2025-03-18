4df0bcfe20e3079fa5638bcbd7a2304a50bbb0ef
prad-human-007 <kanasepradhumn@gmail.com>
2025-03-18 16:15:48 +0530

    completed create class

--- src/app/api/create-class/route.ts
+++ src/app/api/create-class/route.ts
@@ -1,5 +1,48 @@
-export async function POST(req: Request, res: Response) {
-    console.log("Request", req);
-    console.log("Response", res);
-  console.log("Class Created");
+import { NextResponse } from "next/server";
+import { createClient, User } from '@supabase/supabase-js';
+import { v4 as uuidv4 } from 'uuid';
+export const dynamic = 'force-dynamic';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabase = createClient(supabaseUrl!, supabaseKey!);
+let user: User | null  = null;
+
+async function isAuthenticated(req: Request): Promise<boolean> {
+    const token = req.headers.get('Authorization')?.replace('Bearer ', '');
+    if (!token) {
+      return false;
+    }
+  
+    const { data, error } = await supabase.auth.getUser(token);
+    if (error || !data || data.user.role != 'authenticated') {
+      console.error('Failed to authenticate user: ', token, "Error: ", error);  
+      return false;
+    }
+    user = data.user;
+    return true;
+    
+}
+
+
+export async function POST(request: Request) {
+    if(!await isAuthenticated(request) || !user) {
+        return NextResponse.json("Unauthorized", { status: 401 });
+    }
+    const {name, description } = await request.json();
+    console.log("class name:", name, "description:", description, "user:", user.id);
+    
+    const code = uuidv4().slice(0, 5);
+    const {data, error } = await supabase.from('classrooms').insert([{
+        name: name,
+        description: description,
+        code: code,
+        instructor_id: user.id
+    }])
+    console.log("Classroom created", data);
+    if(error) {
+        console.error("Failed to create classroom", error);
+        return NextResponse.json("Failed to create classroom", { status: 500 });
+    }
+    return NextResponse.json({message: 'success'}, { status: 200 });
 }
--- src/components/home/CreateClassButton.tsx
+++ src/components/home/CreateClassButton.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
@@ -10,16 +12,68 @@ import {
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useEffect, useState } from "react"
+import { User } from "@supabase/supabase-js"
+import { createClient } from "@/utils/supabase/client"
+import { useRouter } from "next/navigation"
 
 export function CreateClass() {
+
+  const router = useRouter();
+  const [open, setOpen] = useState(false);
+  const [ user, setUser ] = useState<User | null>(null)
+  const [ token, setToken ] = useState('')
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  // supabase user initialization
+  useEffect(() => {
+      const supabase = createClient();
+      supabase.auth.getSession().then(({ data: {session } , error }) => {
+          if (error || !session?.user) {
+          router.replace("/sign-in"); 
+          } else {
+          setUser(session.user)
+          setToken(session.access_token)
+          setIsAuthenticated(true)
+          }
+      });
+      
+  }, []);
+
+  const createClass = async () => {
+    if(!token) {alert("You are not authenticated"); return}
+    if(!name || !description) {alert("Please fill in all fields"); return}
+    const response = await fetch('/api/create-class',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }, 
+        body: JSON.stringify({
+          name: name,
+          description: description
+        })
+      }
+    )
+    const data = await response.json()
+    console.log("Class Created", data)
+    if (response.ok) {
+      setOpen(false)
+    }
+    else {
+      alert("Failed to create class}")  
+    }
+  }
   return (
-    <Dialog >
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
-        <Button >Edit Profile</Button>
+        <Button variant={"secondary"}>Create Classroom</Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px] bg-white rounded-">
+      <DialogContent className="sm:max-w-[425px]">
         <DialogHeader className="rounded-xl">
-          <DialogTitle>Edit profile</DialogTitle>
+          <DialogTitle>Create Classroom</DialogTitle>
           <DialogDescription>
             Make changes to your profile here. Click save when you're done.
           </DialogDescription>
@@ -27,19 +81,29 @@ export function CreateClass() {
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="name" className="text-right">
-              Name
+              Class Name
             </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+            <Input 
+            id="name"
+            placeholder="Physics" 
+            className="col-span-3"
+            onChange={(e) => setName(e.target.value)}
+             />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="username" className="text-right">
-              Username
+              Description
             </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
+            <Input 
+            id="username" 
+            placeholder="Physics Class for 8th grade" 
+            className="col-span-3" 
+            onChange={(e) => setDescription(e.target.value)}
+            />
           </div>
         </div>
         <DialogFooter>
-          <Button type="submit">Create Class</Button>
+          <Button onClick={createClass} type="submit">Create Class</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
